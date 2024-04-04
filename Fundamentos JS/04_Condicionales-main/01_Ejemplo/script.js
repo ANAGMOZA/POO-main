ed87700452e014b1d10bbe9710413e49449e9ab5
ANAGMOZA <angelmorales@liceovvh.cl>
2024-04-04 12:20:17 -0300

    ok

---
+++ Fundamentos JS/04_Condicionales-main/01_Ejemplo/script.js
@@ -0,0 +1,11 @@
+
+identificarMayorEdad()
+{
+    //identificar si es mayot o menor de edad
+    let edad = prompt("por favor ingrese su Edad: ");
+    if(edad >= 18){
+        alert(`Es mayor de Edad: ${edad} `)
+    } else{
+        alert("Es menor de Edad")
+    }
+}
