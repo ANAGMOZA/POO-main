b99190c119c8d4d8c8004f345a6a8042c83d076e
ANAGMOZA <angelmorales@liceovvh.cl>
2024-04-10 10:14:45 -0400

    ok |

--- Condicionales/01_Ejercicios/script.js
+++ Condicionales/01_Ejercicios/script.js
@@ -1,10 +1,10 @@
 console.log("vinculo")
 
-function agregarNumeros()
+function identificaMayorMenor()
 {
     let resultado =""
-    let num1 = prompt("Agregue el primer numero")
-    let num2 = prompt("Agregue el segundo numero")
+    let num1 = prompt("Agregue el primer numero:")
+    let num2 = prompt("Agregue el segundo numero: ")
     if(num1 > num2){
         resultado = ` El primer numero es mayor: ${num1} - El segundo es menor: ${num2}`
     }else{
