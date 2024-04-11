d809a97fc669035a8b91b9a8ce4b58e3268880ad
ANAGMOZA <angelmorales@liceovvh.cl>
2024-04-11 09:30:29 -0400

    ok

--- Bucles/02_Ejercicios/script.js
+++ Bucles/02_Ejercicios/script.js
@@ -2,12 +2,17 @@ function sumaNumeros() {
     let suma = 0;
     let numero = 1;
     let array = [];
+    let cadena = "";
 
     while (numero <= 10) {
         array.push(numero);
         suma += numero;
+        cadena += numero + " + ";
         numero++;
     }
 
-    document.getElementById(`lista`).innerHTML = `<p>Suma de números del 1 al 10:</p> <br><h2>${suma}</h2>`;
+    cadena = cadena.slice(0, -3) + " = " + suma;
+
+    document.getElementById(`lista`).innerHTML = `<p>Suma de números del 1 al 10:</p> <br><h2>${cadena}</h2>`;
+    document.getElementById(`array`).innerHTML = `<p>Array de números:</p> <br><h2>${array}</h2>`;
 }
