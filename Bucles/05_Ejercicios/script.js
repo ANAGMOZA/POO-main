1e422b729798150d229e52ed0d1e7e58e0573c98
ANAGMOZA <angelmorales@liceovvh.cl>
2024-04-15 11:10:35 -0400

    ok

--- Bucles/05_Ejercicios/script.js
+++ Bucles/05_Ejercicios/script.js
@@ -1,10 +1,8 @@
 function encontrarNumerosPares() {
-    let n = prompt("Ingrese la cantidad de números pares a encontrar");
+    let n = parseFloat(prompt("Ingrese la cantidad de números pares a encontrar"));
     let numerosPares = [];
-    for (let numeros = 0; numerosPares.length < n; numeros++) {
-    if (numeros % 2 === 0) {
-        numerosPares.push(numeros);
-    }
+    for (let i = 1; i <= n; i++) {
+        numerosPares.push(i*2);
     }
     document.getElementById('lista').innerHTML = `<p>Los primeros ${n} números pares son:</p> <br><h2>${numerosPares}</h2>`;
 }
